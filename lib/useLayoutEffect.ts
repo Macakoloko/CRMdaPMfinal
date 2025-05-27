6ef8d99fa6bc20a179b869bae125a1130af15b4a
Macakoloko <gabmondie@gmail.com>
2025-05-27 18:48:22 +0100

    Adiciona função useLayoutUpdateEffect no arquivo useLayoutEffect.ts

--- lib/useLayoutEffect.ts
+++ lib/useLayoutEffect.ts
@@ -8,6 +8,23 @@ const useLayoutEffect = typeof window !== 'undefined'
   ? React.useLayoutEffect 
   : () => {};
 
+/**
+ * Versão do useLayoutEffect que só dispara quando as dependências mudam
+ * Similar ao useUpdateEffect, mas usando useLayoutEffect
+ */
+export const useLayoutUpdateEffect: typeof React.useLayoutEffect = (callback, deps) => {
+  const firstMountRef = React.useRef(true);
+
+  useLayoutEffect(() => {
+    if (firstMountRef.current) {
+      firstMountRef.current = false;
+      return;
+    }
+
+    return callback();
+  }, deps);
+};
+
 export default useLayoutEffect;
 
 /**
